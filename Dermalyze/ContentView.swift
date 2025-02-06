cac49f3f7592b7821fa283f8b7702a91a23d3664
Sachh Moka <sachhmoka@gmail.com>
2025-02-06 21:13:00 +1000

    Initial Commit

---
+++ Dermalyze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dermalyze
+//
+//  Created by samo on 6/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dermalyze/DermalyzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DermalyzeApp.swift
+//  Dermalyze
+//
+//  Created by samo on 6/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct DermalyzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
